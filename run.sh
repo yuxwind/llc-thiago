7b589d6ab78a9ba2bba729cf403b35a99b1a98dc
Xin Yu <yuxwind@gmail.com>
2021-05-20 16:18:50 -0600

    fix minor bug in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 net=$1
-sh train1_net{$net}.sh > logs/train1_net{$net}.log 2>&1 &
-sh train2_net{$net}.sh > logs/train2_net{$net}.log 2>&1 &
-sh train3_net{$net}.sh > logs/train3_net{$net}.log 2>&1 &
-sh train4_net{$net}.sh > logs/train4_net{$net}.log 2>&1 &
-sh train5_net{$net}.sh > logs/train5_net{$net}.log 2>&1 &
+sh train1_net${net}.sh > logs/train1_net${net}.log 2>&1 &
+sh train2_net${net}.sh > logs/train2_net${net}.log 2>&1 &
+sh train3_net${net}.sh > logs/train3_net${net}.log 2>&1 &
+sh train4_net${net}.sh > logs/train4_net${net}.log 2>&1 &
+sh train5_net${net}.sh > logs/train5_net${net}.log 2>&1 &
