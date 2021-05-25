9ca44c43cd9ba1a6fde9415a1cde7cfb7a852521
xiyu <xiyu@lab1-1.eng.utah.edu>
2021-05-25 17:11:38 -0600

    run training scripts on the background

--- train_cifar100_rgb.py
+++ train_cifar100_rgb.py
@@ -16,6 +16,5 @@ script = scripts[ind]
 env = "GRB_LICENSE_FILE=~/gurobi-license/`sh ~/get_uname.sh`/gurobi.lic "
 log = mkpath(os.path.join(f"logs/{info}/{os.path.basename(script).replace('.sh', '.log')}"))
 
-print(f"{env} sh {script}")
-os.system(f"{env} sh {script}")
-
+print(f"{log}")
+os.system(f"{env} sh {script} > {log} 2>&1 &")
